925778812ac3165d4c62a6fdc545759dbb4c946f
Tro Armen <troarmen@gmail.com>
2024-12-17 22:38:33 -0300

    phonenumber

--- java/PhoneNumber.java
+++ java/PhoneNumber.java
@@ -8,7 +8,7 @@ The returned format must be correct in order to complete this challenge.
 Don't forget the space after the closing parentheses!
 */
 
-public class PhoneNumber {
+public class PhoneNumber{
     public static void main(String[] args) {
         System.out.println(createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0})); // => returns "(123) 456-7890"
     }
